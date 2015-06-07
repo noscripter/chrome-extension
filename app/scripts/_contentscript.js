1cd3ecf4ed8f883465bb94665ac38321a16f0a49
Stefan Buck <github@stefanbuck.com>
2015-06-07 20:00:47 +0200

    prepare upcoming GitHub-Linker version

--- app/scripts/_contentscript.js
+++ app/scripts/_contentscript.js
@@ -1,31 +1,33 @@
 'use strict';
 
-var gitHubLinkerCore = require('github-linker-core');
+// var gitHubLinkerCore = require('github-linker-core');
+var gitHubLinkerCore = require('./core/index.js');
+
 var $ = require('jquery');
 window.$ = $;
 
 chrome.storage.sync.get(['version', 'enableUpdateNotification'], function(store) {
-    var pkgVersion, options;
+  var pkgVersion, options;
 
-    if (store.enableUpdateNotification === undefined) {
-        store.enableUpdateNotification = true;
+  if (store.enableUpdateNotification === undefined) {
+    store.enableUpdateNotification = true;
+  }
+
+  pkgVersion = require('../manifest.json').version.split('.').slice(0,-1).join('.');
+  options = {
+    changelog: 'https://github.com/github-linker/chrome-extension/releases'
+  };
+
+  if (store.enableUpdateNotification && store.version && store.version !== pkgVersion) {
+    options.showUpdateNotification = true;
+  }
+
+  store.version = pkgVersion;
+  chrome.storage.sync.set(store);
+
+  gitHubLinkerCore(window, options, function(err) {
+    if (err) {
+      console.error(err);
     }
-
-    pkgVersion = require('../manifest.json').version.split('.').slice(0,-1).join('.');
-    options = {
-        changelog: 'https://github.com/github-linker/chrome-extension/releases'
-    };
-
-    if (store.enableUpdateNotification && store.version && store.version !== pkgVersion) {
-        options.showUpdateNotification = true;
-    }
-
-    store.version = pkgVersion;
-    chrome.storage.sync.set(store);
-
-    gitHubLinkerCore(window, options, function(err) {
-        if (err) {
-            console.error(err);
-        }
-    });
+  });
 });
--- app/scripts/_options.js
+++ app/scripts/_options.js
@@ -3,55 +3,55 @@
 var $ = require('jquery');
 
 function initMenu() {
-    $('.menu a').click(function(ev) {
+  $('.menu a').click(function(ev) {
     ev.preventDefault();
     var selected = 'selected';
 
     $('.mainview > *').removeClass(selected);
     $('.menu li').removeClass(selected);
     setTimeout(function() {
-     $('.mainview > *:not(.selected)').css('display', 'none');
+      $('.mainview > *:not(.selected)').css('display', 'none');
     }, 100);
 
     $(ev.currentTarget).parent().addClass(selected);
     var currentView = $($(ev.currentTarget).attr('href'));
     currentView.css('display', 'block');
     setTimeout(function() {
-     currentView.addClass(selected);
+      currentView.addClass(selected);
     }, 0);
 
     setTimeout(function() {
-     $('body')[0].scrollTop = 0;
+      $('body')[0].scrollTop = 0;
     }, 200);
-    });
+  });
 }
 
 function setStaticContent() {
-    $('#version-container').html('Version ' +require('../manifest.json').version);
+  $('#version-container').html('Version ' +require('../manifest.json').version);
 }
 
 function initSettingStorage () {
-    var storage = chrome.storage.sync;
-    var $enableUpdateNotification = $('#enable_update_notification')
+  var storage = chrome.storage.sync;
+  var $enableUpdateNotification = $('#enable_update_notification');
 
-    function loadSettings() {
-        storage.get('enableUpdateNotification', function(val) {
-            $enableUpdateNotification.prop('checked', (typeof val.enableUpdateNotification === 'undefined') ? true : val.enableUpdateNotification);
-        });
-    }
+  function loadSettings() {
+    storage.get('enableUpdateNotification', function(val) {
+      $enableUpdateNotification.prop('checked', (typeof val.enableUpdateNotification === 'undefined') ? true : val.enableUpdateNotification);
+    });
+  }
 
-    function saveSettings() {
-        storage.set({
-            enableUpdateNotification: $enableUpdateNotification.is(':checked'),
-        });
-    }
+  function saveSettings() {
+    storage.set({
+      enableUpdateNotification: $enableUpdateNotification.is(':checked'),
+    });
+  }
 
-    $enableUpdateNotification.on('change', saveSettings);
-    loadSettings();
+  $enableUpdateNotification.on('change', saveSettings);
+  loadSettings();
 }
 
 $(function() {
-    initMenu();
-    initSettingStorage();
-    setStaticContent();
+  initMenu();
+  initSettingStorage();
+  setStaticContent();
 });
